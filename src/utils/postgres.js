16595a02c0983e8531782e74db88f7b498ef4a5a
marcossilva11 <marcosant2170@gmail.com>
2025-01-19 15:37:27 -0300

    19/01 - alterando conexão com banco de dados e adicionando .env no projeto

--- src/utils/postgres.js
+++ src/utils/postgres.js
@@ -1,11 +1,28 @@
-import pg from "pg";
+import "dotenv/config";
+import pkg from "pg";
 
-export const db = new pg.Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'bd_bibliotec',
-  password: 'root',
-  port: 5432,
+const { Pool } = pkg;
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: String(process.env.DB_PASSWORD),
+  port: process.env.DB_PORT,
 });
 
-db.connect();
+// Função para executar uma conexão com o pool
+export const query = async (text, params) => {
+  const client = await pool.connect(); // Pega uma conexão do pool 'emprestada'
+  try {
+    const res = await client.query(text, params); // Executa uma conexão com o texto e os parametros passados pelo servidor
+    return res; //Retorna o resultado da requisição
+  } catch (err) {
+    console.log("Query error: ", err);
+    throw err; // Propaga o erro para trata-lo quando a requisição é feita pelo servidor
+  } finally {
+    client.release(); // 'Devolve' a conexão de volta para o pool
+  }
+};
+
+export default pool; // Exporta o pool para caso seja necessario utiliza-lo
